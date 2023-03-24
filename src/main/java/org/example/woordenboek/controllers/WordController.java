1e70ddb2551e9691e958fcedd6333c57b44ab95a
Jussi Ccoyllo <jussi.ccoyllo@gmail.com>
2023-03-24 23:05:37 +0100

    Controllers

--- src/main/java/org/example/woordenboek/controllers/WordController.java
+++ src/main/java/org/example/woordenboek/controllers/WordController.java
@@ -1,5 +1,12 @@
 package org.example.woordenboek.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/words")
 public class WordController {
+
+
 
 }
